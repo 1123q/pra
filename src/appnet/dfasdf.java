4a3c7fb2a4c52442b95c0892f40447224ea9d3d3
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2022-08-21 16:03:15 -0500

    Second cambio

---
+++ src/appnet/dfasdf.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appnet;
+
+/**
+ *
+ * @author DELL
+ */
+public class dfasdf {
+    String hola;
+    int ho;
+
+    public void setHola(String hola) {
+        this.hola = hola;
+    }
+
+    public void setHo(int ho) {
+        this.ho = ho;
+    }
+    
+}
